4d71bb167a11f9cea2851df8c7e4dce3658ee7f0
mahmodDAHOL <mahmodaldahol010@gmail.com>
2023-12-23 17:48:21 +0300

    hospital module with its icon

---
+++ hospital/__manifest__.py
@@ -0,0 +1,27 @@
+{
+    'name': 'Hospital Management',
+    'version': '1.0',
+    'description': 'Hospital management system',
+    'summary': 'Hospital management system',
+    'sequence': '1000',
+    'author': 'Mahmod Aldahol',
+    'website': '',
+    'license': 'LGPL-3',
+    'category': 'Hospital',
+    'depends': [
+        
+    ],
+    'data': [
+        ''
+    ],
+    'demo': [
+        ''
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'assets': {
+        
+    }
+}
+
