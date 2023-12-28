1c5cbc0231a7e5a32250e6534934bf89ca22a3db
mahmodDAHOL <mahmodaldahol010@gmail.com>
2023-12-29 00:02:56 +0300

    add gender to appointment model as related field and make age as computed field and use api.depends() decorator to make age depends on date_of_birth field

--- hospital/models/appointment.py
+++ hospital/models/appointment.py
@@ -6,5 +6,6 @@ class HospitalAppointment(models.Model):
     _description = 'Hospital appointment'
     
     patient_id = fields.Many2one('hospital.patient', string='Patient')
+    gender = fields.Selection(related='patient_id.gender')
     appointment_time = fields.Datetime(string='Appointment time', default=fields.Datetime.now())
     booking_date = fields.Datetime(string='Booking date', default=fields.Date.context_today) 
--- hospital/models/patient.py
+++ hospital/models/patient.py
@@ -1,4 +1,7 @@
-from odoo import fields, models
+from datetime import date
+
+from odoo import api, fields, models
+
 
 class HospitalPatient(models.Model):
     _inherit = 'mail.thread'
@@ -6,6 +9,16 @@ class HospitalPatient(models.Model):
     _description = 'Hospital Patient'
     
     name = fields.Text(string='Name', tracking=True)
-    age = fields.Integer(string='Age', tracking=True)
+    date_of_birth = fields.Date(string='Data of birth')
+    age = fields.Integer(string='Age', compute= '_compute_age', tracking=True)
     gender = fields.Selection([('male','Male'),('female','Female')], string='Gender', tracking=True)
     active  = fields.Boolean(string="Active", default=True)
+    
+    @api.depends('date_of_birth')
+    def _compute_age(self):
+        for patient in self:
+            if patient.date_of_birth:
+                today = date.today()
+                patient.age = today.year -patient.date_of_birth.year
+            else:
+                patient.age = 0
