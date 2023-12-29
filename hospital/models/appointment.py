6f18a97f0317fe26373c7462129b17788fcf6d07
mahmodDAHOL <mahmodaldahol010@gmail.com>
2023-12-29 17:43:13 +0300

    add prescription,state and priority fields and add status bar for appointment model

--- hospital/models/appointment.py
+++ hospital/models/appointment.py
@@ -4,8 +4,22 @@ class HospitalAppointment(models.Model):
     _inherit = 'mail.thread'
     _name = 'hospital.appointment'
     _description = 'Hospital appointment'
+    _rec_name = 'patient_id'
     
     patient_id = fields.Many2one('hospital.patient', string='Patient')
     gender = fields.Selection(related='patient_id.gender')
     appointment_time = fields.Datetime(string='Appointment time', default=fields.Datetime.now())
     booking_date = fields.Datetime(string='Booking date', default=fields.Date.context_today) 
+    prescription = fields.Html(string='Prescription')
+    priority = fields.Selection([
+        ('0','Normal'),
+        ('1','Low'),
+        ('2','High'),
+        ('3','Very High')], string="Priority"
+    )
+    state = fields.Selection([
+        ('draft','Draft'),
+        ('in_consultation','In Consultation'),
+        ('done','Done'),
+        ('cancel','Cancelled')], string="Status"
+    )
