ddd324336a963706dd177e4e08f4d624627a9e62
mahmodDAHOL <mahmodaldahol010@gmail.com>
2024-01-06 16:34:25 +0300

    add color widget to patient tag

--- hospital/models/patient_tag.py
+++ hospital/models/patient_tag.py
@@ -7,3 +7,5 @@ class PatientTag(models.Model):
     
     name = fields.Text(string='Name', required=True, tracking=True)
     active  = fields.Boolean(string="Active", default=True)
+    color = fields.Integer(string="Color")
+    color_2 = fields.Char(string="Color_2")
