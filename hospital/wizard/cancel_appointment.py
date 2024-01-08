c951024da7e0883d348b7ec96b5ac9c9ddd524d3
mahmodDAHOL <mahmodaldahol010@gmail.com>
2024-01-08 15:40:41 +0300

    using get_default to set default value for cancel appointment date

--- hospital/wizard/cancel_appointment.py
+++ hospital/wizard/cancel_appointment.py
@@ -1,12 +1,20 @@
-from odoo import fields, models
+import datetime
+from odoo import fields, models, api
 
 
 class CancelAppointmentWizard(models.TransientModel):
     _name = 'cancel.appointment.wizard'
     _description = 'Cancel Appointment Wizard'
     
+    @api.model
+    def default_get(self, fields_list):
+        res = super(CancelAppointmentWizard, self).default_get(fields_list)
+        res['cancel_date'] = datetime.date.today()
+        return res
+
     appointment_id = fields.Many2one('hospital.appointment', string="Appointment")
     reason = fields.Text(string="Reason")
+    cancel_date = fields.Text(string="Cancellation Date")
     
     def cancel_action(self):
         self.appointment_id.state = "cancel"
