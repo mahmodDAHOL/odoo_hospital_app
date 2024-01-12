1603d838476b21879c9b28bff80282c3248a49f5
mahmodDAHOL <mahmodaldahol010@gmail.com>
2024-01-12 22:46:39 +0300

    add duplication behaviour to patient tag model

--- hospital/models/patient_tag.py
+++ hospital/models/patient_tag.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class PatientTag(models.Model):
@@ -6,10 +6,19 @@ class PatientTag(models.Model):
     _description = 'Patient Tag'
     
     name = fields.Text(string='Name', required=True, tracking=True)
-    active  = fields.Boolean(string="Active", default=True)
+    active  = fields.Boolean(string="Active", default=True, copy=False)
     color = fields.Integer(string="Color")
     color_2 = fields.Char(string="Color_2")
     sequence = fields.Integer(string="Sequence")
+    
+    @api.returns('self', lambda value: value.id)
+    def copy(self, default=None):
+        if default is None:
+            default = {}
+        if not default.get('name'):
+            default['name'] = f"{self.name} (copy)"
+        default['sequence'] = 10
+        return super(PatientTag, self).copy(default)
 
     _sql_constraints = [
         ('unique_tag_name','unique (name, active)','Name must be unique'),
