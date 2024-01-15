6213c3759e20c87f7950e52083eb728c9fc10378
mahmodDAHOL <mahmodaldahol010@gmail.com>
2024-01-15 15:19:29 +0300

    add edit button for appointment group by patient id for appointment tree view

--- hospital/models/patient.py
+++ hospital/models/patient.py
@@ -1,6 +1,7 @@
 from datetime import date
 
 from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class HospitalPatient(models.Model):
@@ -13,14 +14,29 @@ class HospitalPatient(models.Model):
     ref = fields.Char(string="Reference")
     age = fields.Integer(string='Age', compute= '_compute_age', tracking=True)
     gender = fields.Selection([('male','Male'),('female','Female')], string='Gender', tracking=True)
+    appointment_id = fields.Many2one('hospital.appointment', string="Appointment")
     active  = fields.Boolean(string="Active", default=True)
     image  = fields.Image(string="Image")
+    tag_ids = fields.Many2many('patient.tag', string="Tags")
+    appointment_count = fields.Integer(string='Appointment Count', compute= '_compute_appointment_count', store=True)
+    appointment_ids = fields.One2many('hospital.appointment', 'patient_id', string='Appointment')
     parent = fields.Char(string='Parent')
     marital_status = fields.Selection([
         ('married','Married'),
         ('single','Single'),
     ], string="Marital", tracking=True)
-    partner_name = fields.Char(string="Parent name")
+    partner_name = fields.Char(string="Partner name")
+
+    @api.depends('appointment_ids')
+    def _compute_appointment_count(self):
+        for rec in self:
+            rec.appointment_count = self.env['hospital.appointment'].search_count([('patient_id', '=', rec.id)])
+
+    api.ondelete(at_uninstall=False)
+    def check_appointments(self):
+        for rec in self:
+            if rec.appointment_ids:
+                raise ValidationError("You cannot delete a patient with appointments.")
 
     @api.model
     def create(self, vals):
